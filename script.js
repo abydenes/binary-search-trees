3fb6e49b848323a2d00b3e69a5b6a22c950d9fa8
abydenes <schoolreef2@protonmail.com>
2022-12-25 09:25:29 +0300

    Add createBST function

--- script.js
+++ script.js
@@ -13,3 +13,51 @@ class Node {
     this.right = node;
   }
 }
+
+class Tree {
+  constructor(root, array) {
+    this.root = root;
+  }
+}
+
+let arr = [1, 3, 4, 5, 7, 8, 9, 23, 67, 324, 6345];
+
+function createBST(arr, start = 0, end = arr.length - 1) {
+  if (start > end) return null;
+  let mid = Math.ceil((start + end) / 2);
+  let root = new Node(arr[mid]);
+
+  root.setLeft(createBST(arr, start, mid - 1));
+  root.setRight(createBST(arr, mid + 1, end));
+
+  return root;
+}
+
+function insertNode() {}
+
+function deleteNode() {}
+
+function find() {}
+
+function levelOrder() {}
+
+function inorder() {}
+
+function preorder() {}
+
+function postorder() {}
+
+function height() {}
+
+const prettyPrint = (node, prefix = "", isLeft = true) => {
+  if (node.right !== null) {
+    prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+  }
+  console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+  if (node.left !== null) {
+    prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+  }
+};
+
+let tree1 = createBST(arr);
+prettyPrint(tree1);
