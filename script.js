8c18f352cf905c69b96b8b1638701691eca1ed6c
abydenes <schoolreef2@protonmail.com>
2022-12-28 21:34:45 +0300

    Add the remaining functions

--- script.js
+++ script.js
@@ -20,7 +20,9 @@ class Tree {
   }
 }
 
-let arr = [1, 3, 4, 5, 7, 8, 9, 23, 67, 324, 6345];
+let arr2 = [1, 3, 4, 5, 7, 8, 9, 23, 67, 324, 6345];
+let tree1 = createBST(arr2);
+prettyPrint(tree1);
 
 function createBST(arr, start = 0, end = arr.length - 1) {
   if (start > end) return null;
@@ -96,9 +98,9 @@ function findMin(tree) {
   } else return findMin(tree.left);
 }
 
-function levelOrder(tree1, cb) {
+function levelOrder(tree, cb) {
   let queue = [];
-  queue.push(tree1);
+  queue.push(tree);
   while (queue.length) {
     let current = queue.shift();
     cb(current);
@@ -107,15 +109,73 @@ function levelOrder(tree1, cb) {
   }
 }
 
-function inorder() {}
+function inorder(tree, cb) {
+  if (tree === null) {
+    return;
+  } else {
+    inorder(tree.left, cb);
+    cb(tree);
+    inorder(tree.right, cb);
+  }
+}
 
-function preorder() {}
+function preorder(tree, cb) {
+  if (tree === null) {
+    return;
+  } else {
+    cb(tree);
+    preorder(tree.left, cb);
+    preorder(tree.right, cb);
+  }
+}
 
-function postorder() {}
+function postorder(tree, cb) {
+  if (tree === null) {
+    return;
+  } else {
+    postorder(tree.left, cb);
+    postorder(tree.right, cb);
+    cb(tree);
+  }
+}
 
-function height() {}
+function height(tree) {
+  if (tree === null) {
+    return 0;
+  }
 
-const prettyPrint = (node, prefix = "", isLeft = true) => {
+  let leftHeight = height(tree.left);
+  let rightHeight = height(tree.right);
+  return Math.max(leftHeight, rightHeight) + 1;
+}
+
+// I don't really understand the function below :(
+function depth(tree, value) {
+  if (tree == null) {
+    return -1;
+  }
+
+  if (tree.value == value) {
+    return 0;
+  }
+
+  let leftDepth = depth(tree.left, value);
+  let rightDepth = depth(tree.right, value);
+
+  if (leftDepth == -1 && rightDepth == -1) {
+    return -1;
+  } else if (leftDepth != -1) {
+    return 1 + leftDepth;
+  } else {
+    return 1 + rightDepth;
+  }
+}
+
+function isBalanced(tree) {
+  return height(tree.left) === height(tree.right);
+}
+
+function prettyPrint(node, prefix = "", isLeft = true) {
   if (node.right !== null) {
     prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
   }
@@ -123,7 +183,53 @@ const prettyPrint = (node, prefix = "", isLeft = true) => {
   if (node.left !== null) {
     prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
   }
-};
+}
 
-let tree1 = createBST(arr);
-prettyPrint(tree1);
+function tieItAllTogether(length, min, max) {
+  let array = createRandomNumAr(length, min, max);
+  let tree = createBST(array);
+  printSequence(tree);
+  insertNode(tree, 113);
+  insertNode(tree, 266);
+  insertNode(tree, 335);
+  insertNode(tree, 590);
+  insertNode(tree, 591);
+  insertNode(tree, 592);
+  insertNode(tree, 593);
+  insertNode(tree, 594);
+  insertNode(tree, 595);
+  tree = rebalance(tree);
+  printSequence(tree);
+}
+
+function printSequence(tree) {
+  console.log("Printing tree in level order...");
+  levelOrder(tree, console.log);
+  console.log("Printing tree in preorder...");
+  preorder(tree, console.log);
+  console.log("Printing tree in postorder...");
+  postorder(tree, console.log);
+  console.log("Printing tree in inorder...");
+  inorder(tree, console.log);
+  prettyPrint(tree);
+  console.log(`Is tree balanced? = ${isBalanced(tree)}`);
+}
+
+function createRandomNumAr(length, min, max) {
+  let arr = [];
+  for (let i = 0; i < length; i++) {
+    arr.push(Math.floor(Math.random() * (max - min) + min));
+  }
+  return removeDuplicatesAndSort(arr);
+}
+
+function removeDuplicatesAndSort(array) {
+  return [...new Set(array)].sort();
+}
+
+function rebalance(tree) {
+  let arr3 = [];
+  const push = (item) => arr3.push(item.value);
+  inorder(tree, push);
+  return createBST(arr3);
+}
